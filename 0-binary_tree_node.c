4eca3d1ae11ad66d318016b6d5e85a38aa494f41
ayo <ayobamidele006@gmail.com>
2023-03-28 18:16:12 +0100

    [fix]: fixed betty errors with curly braces and newline

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,7 +8,8 @@
  *  Return: Null if malloc failed else new_node created
  */
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value){
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
 	binary_tree_t *new_node;
 
 	new_node = malloc(sizeof(binary_tree_t));

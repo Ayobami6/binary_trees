af082e21fb3e855138560024706e1fb8eab02f22
ayo <ayobamidele006@gmail.com>
2023-03-31 05:35:29 +0100

    [implementation/tasks]: added task 07-010 solutions

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,6 +1,5 @@
 #include "binary_trees.h"
 
-
 /**
  * binary_tree_node - binary tree node function
  * @parent: parent node
@@ -8,7 +7,7 @@
  *  Return: Null if malloc failed else new_node created
  */
 
-binary_tree_t *binary_tree_node(binary_tree_t *head, int value)
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
@@ -17,10 +16,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *head, int value)
 		return (NULL);
 
 	new_node->n = value;
-	new_node->parent = head;
+	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = NULL;
 
 	return (new_node);
-
 }
